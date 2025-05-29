443188cab1014549817742f535018431f4d9ff22
Furqan <shahsyed216@gmail.com>
2025-05-30 02:26:59 +0500

    terraform module created

---
+++ infra-demo-1/dev/vpc/provider.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "us-east-1"
+
+  profile = "codecirrus-dev"
+}
+
+terraform {
+  required_version = ">= 1.0.0"
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "terraform-state-codecirrus"
+    key            = "infra-demo-1/dev/vpc/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "codecirrus-terraform-locks"
+  }
+}
